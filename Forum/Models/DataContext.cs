fd84e8d924a51ed2f9d6e441785edd21dc0c126e
Anna Axelsson <a.axelsson51@gmail.com>
2023-09-26 20:30:04 +0200

    Add initial files and DataContext

---
+++ Forum/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Forum.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
+
+
+
