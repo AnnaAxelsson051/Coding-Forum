2884edb7cbbb3aaabe0f096b887a879ee978b98c
Anna Axelsson <a.axelsson51@gmail.com>
2023-09-27 12:35:18 +0200

    Modify homecontroller and index.html

--- Forum/Controllers/HomeController.cs
+++ Forum/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Forum.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Forum.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DataContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
