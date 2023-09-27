09d9045f0c7a5270e35e2825c9e2f711ce640b42
Anna Axelsson <a.axelsson51@gmail.com>
2023-09-27 17:37:19 +0200

    Add DB helper

---
+++ Forum/Models/Helper/DBhelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Forum.Models.Helper
+{
+	public class DBhelper
+	{
+        private readonly DataContext _context;
+
+        public DBhelper(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
+
