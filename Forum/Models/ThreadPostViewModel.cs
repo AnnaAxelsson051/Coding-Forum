160b2d50faedc26d74a279a393bade9f938aabed
Anna Axelsson <a.axelsson51@gmail.com>
2023-09-27 18:04:31 +0200

    Updat postviewmodel

--- Forum/Models/ThreadPostViewModel.cs
+++ Forum/Models/ThreadPostViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
 namespace Forum.Models
 {
 	public class ThreadPostViewModel
 	{
-		public ThreadPostViewModel()
-		{
-		}
-	}
+        public string? ThreadHeading { get; set; }
+
+        public string? PostTitle { get; set; }
+
+        public string TextBody { get; set; }
+
+    }
 }
 
