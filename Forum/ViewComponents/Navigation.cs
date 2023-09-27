47a777592075e9eb433cce1d6e7d76076d74f0c6
Anna Axelsson <a.axelsson51@gmail.com>
2023-09-27 16:51:00 +0200

    Add Navigation

---
+++ Forum/ViewComponents/Navigation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Forum.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Forum.ViewComponents
+{
+   // similar to a partial view but can have its
+   //own logic, encapsulated in a class, and can
+   //be invoked from a view.
+
+
+    public class Navigation 
+	{
+        private readonly DataContext _context;
+
+        public Navigation(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
+
